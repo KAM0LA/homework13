25ea8e5ec330ec02a64b1d467ea3d7c68689a23f
Kamola0911 <kamola092002@mail.ru>
2023-04-20 15:40:11 +0500

    izoh

--- js/main.js
+++ js/main.js
@@ -17,10 +17,10 @@ function shrink() {
 let modeBtn = document.getElementById("dark-light");
 
 modeBtn.addEventListener("click", function () {
-  if (document.body.className != "dark") {
-    this.firstElementChild.src = "images/light.svg";
-  } else {
+  if (document.body.className != "light") {
     this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.svg";
   }
-  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
 });
